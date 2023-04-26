e48ea85aa3809c695d1aebd482af9d7fd74b7b57
Brian Addicks <brian@addicks.us>
2023-04-26 16:06:29 -0400

    try with variables

--- php-app/src/db.php
+++ php-app/src/db.php
@@ -8,11 +8,16 @@ Author: VINEET KUMAR
 # var_dump(getenv('MY_ENVVAR'));
 # $admin = getenv("mysql_db_name");
 #    echo $admin;
-define('DB_USERNAME',getenv('MYSQL_USERNAME'));
-define('DB_PASSWORD',getenv('MYSQL_PASSWORD'));
-define('DB_HOST',getenv('MYSQL_HOST'));
-#define('DB_PORT',getenv('MYSQL_PORT'));
-define('DB_DATABASE',getenv('MYSQL_DATABASE'));
+// define('DB_USERNAME',getenv('MYSQL_USERNAME'));
+// define('DB_PASSWORD',getenv('MYSQL_PASSWORD'));
+// define('DB_HOST',getenv('MYSQL_HOST'));
+// #define('DB_PORT',getenv('MYSQL_PORT'));
+// define('DB_DATABASE',getenv('MYSQL_DATABASE'));
+
+$db_hostname = parse_url(getenv('MYSQL_HOST'));
+$db_user = parse_url(getenv('MYSQL_USERNAME'));
+$db_password = parse_url(getenv('MYSQL_PASSWORD'));
+$db_database = parse_url(getenv('MYSQL_DATABASE'));
 #$host = GETENV('MYSQL_HOST');
 #$port = getenv('MYSQL_PORT');
 #$db   = $_ENV['MYSQL_USERNAME'];
@@ -20,11 +25,11 @@ define('DB_DATABASE',getenv('MYSQL_DATABASE'));
 #echo $username;
 #$password = getenv('MYSQL_PASSWORD');
 #echo $host."<\br>".$db."<\br>".DB_USERNAME."<\br>".$password;
-$con = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+$con = mysqli_connect($db_hostname,$db_user,$db_password,$db_database);
 #$con = mysqli_connect(localhost,tests,tests,register);
 // Check connection
 if (mysqli_connect_errno())
   {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error() . DB_HOST;
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 ?>
