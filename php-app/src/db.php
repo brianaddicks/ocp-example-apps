1b0338db50145d35f4eb054c1f65a8fc5a92fe41
Brian Addicks <brian@addicks.us>
2023-04-27 09:15:13 -0400

    remove debug output

--- php-app/src/db.php
+++ php-app/src/db.php
@@ -1,23 +1,19 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
 /*
 Author: VINEET KUMAR
  */
 # var_dump(getenv('MY_ENVVAR'));
 # $admin = getenv("mysql_db_name");
 #    echo $admin;
-// define('DB_USERNAME',getenv('MYSQL_USERNAME'));
-// define('DB_PASSWORD',getenv('MYSQL_PASSWORD'));
-// define('DB_HOST',getenv('MYSQL_HOST'));
-// #define('DB_PORT',getenv('MYSQL_PORT'));
-// define('DB_DATABASE',getenv('MYSQL_DATABASE'));
+define('DB_USERNAME',getenv('MYSQL_USERNAME'));
+define('DB_PASSWORD',getenv('MYSQL_PASSWORD'));
+define('DB_HOST',getenv('MYSQL_HOST'));
+#define('DB_PORT',getenv('MYSQL_PORT'));
+define('DB_DATABASE',getenv('MYSQL_DATABASE'));
 
-$db_hostname = getenv('MYSQL_HOST', true);
-$db_user = getenv('MYSQL_USERNAME');
-$db_password = getenv('MYSQL_PASSWORD');
-$db_database = $_ENV['MYSQL_DATABASE'];
 #$host = GETENV('MYSQL_HOST');
 #$port = getenv('MYSQL_PORT');
 #$db   = $_ENV['MYSQL_USERNAME'];
@@ -25,8 +21,8 @@ $db_database = $_ENV['MYSQL_DATABASE'];
 #echo $username;
 #$password = getenv('MYSQL_PASSWORD');
 #echo $host."<\br>".$db."<\br>".DB_USERNAME."<\br>".$password;
-var_dump( get_defined_vars() );
-$con = mysqli_connect($db_hostname,$db_user,$db_password,$db_database);
+// var_dump( get_defined_vars() );
+$con = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 #$con = mysqli_connect(localhost,tests,tests,register);
 // Check connection
 if (mysqli_connect_errno()) {
